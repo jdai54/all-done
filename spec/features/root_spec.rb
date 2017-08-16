2fe7a0bc26b662cb98d01755d88f25952674cf35
Jake <jdai54@gmail.com>
2017-08-16 03:14:58 -0700

    Corrected RSpec issue but second test not passing

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 describe "user visits the root page", :type => :feature do
   it "takes them to the profile page" do
     visit root_path
-    expect(page).to have_content 'What will you accomplish today?'
+    expect(page).to have_content 'Log in'
   end
 end
 
 describe "user signs in", :type => :feature do
   let(:user) { create(:user, email: 'user@example.com', password: 'password', password_confirmation: 'password') }
   it "successfully" do
-    visit user_signin_path
+    visit user_session_path
     fill_in 'Email', with: 'user@example.com'
-    fill_in 'Password', with 'password'
-    click 'Log in'
+    fill_in 'Password', with: 'password'
+    click_on 'Log in'
     expect(page).to have_content 'Sign Out'
   end
 
   it "unsuccessfully" do
-    visit user_signin_path
-    expect(page).to have_content 'What will you accomplish today?'
+    visit new_user_session_path
+    expect(page).to have_content 'Log in'
   end
 end
