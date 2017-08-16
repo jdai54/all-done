187410e4aba7b95f87e2ee70882ad30acfdb00ac
Jake <jdai54@gmail.com>
2017-08-16 08:49:49 -0700

    RSpec tests corrected and passing

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -11,8 +11,8 @@ describe "user signs in", :type => :feature do
   let(:user) { create(:user, email: 'user@example.com', password: 'password', password_confirmation: 'password') }
   it "successfully" do
     visit user_session_path
-    fill_in 'Email', with: 'user@example.com'
-    fill_in 'Password', with: 'password'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
     click_on 'Log in'
     expect(page).to have_content 'Sign Out'
   end
