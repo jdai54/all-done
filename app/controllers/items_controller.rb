9bb9d84a9c0a65192897f3460535ae597154fa32
Jake <jdai54@gmail.com>
2017-09-21 18:12:18 -0700

    Added Ajax for deleting items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,24 +3,18 @@ class ItemsController < ApplicationController
     @user = User.find(current_user.id)
     @item = Item.new( { name: params[:item][:name], user_id: current_user.id } )
 
-    if @item.save
-      flash[:success] = "Item was saved."
-      redirect_to user_path(@user.id)
-    else
-      flash[:alert] = "There was an error saving the item. Please try again."
-      redirect_to user_path(@user.id)
-    end
+    @item.save
+    redirect_to user_path(@user.id)
   end
 
   def destroy
     @user = User.find(params[:user_id])
     @item = Item.find(params[:id])
-    
-    if @item.destroy
-      flash[:notice] = "\"#{@item.name}\" was deleted."
-    else
-      flash[:now] = "There was an error trying to delete the task. Please try again."
+    @item.destroy
+
+    respond_to do |format|
+      format.html
+      format.js
     end
-    redirect_to root_path
   end
 end
